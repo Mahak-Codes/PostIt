3e38ce59aeb53adaebe9b07ae0aceba8a6ba6932
Mahak-Codes <mahakg2311@gmail.com>
2025-07-24 18:37:11 +0530

    Post Request

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const path=require("path");
 const app=express();
 const port=8080;
 const posts=require("./views/data.js");
-app.set("view engine","views");
+const fs = require("fs");
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
-app.use(express.urlencoded({extented:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.get("/",(req,res)=>{
@@ -14,6 +15,20 @@ app.get("/",(req,res)=>{
 
 app.get("/posts",(req,res)=>{
   res.render("index.ejs",{posts});
+})
+app.get("/posts/new",(req,res)=>{
+  res.render("addpost.ejs");
+})
+app.post("/posts",(req,res)=>{
+  const { user, url, content } = req.body;
+  posts.push({
+    username: user,
+    image: url,
+    content: content
+  });
+  const updatedData = `let posts = ${JSON.stringify(posts, null, 2)};\n\nmodule.exports = posts;`;
+  fs.writeFileSync(path.join(__dirname, "views", "data.js"), updatedData);
+  res.redirect("/posts");
 })
 app.listen(port,(req,res)=>{
   console.log(`Server is listening  on ${port}`);
--- views/data.js
+++ views/data.js
@@ -1,44 +1,49 @@
 let posts = [
   {
-    username: "mahak.codes",
-    content: "Super pumped to contribute to open source this month! 💻🔥 #GirlWhoCodes",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOwO3JZp95BH5nnOB7i_-Ssqx2EAtdOXS2g&s" 
+    "username": "mahak.codes",
+    "content": "Super pumped to contribute to open source this month! 💻🔥 #GirlWhoCodes",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThOwO3JZp95BH5nnOB7i_-Ssqx2EAtdOXS2g&s"
   },
   {
-    username: "ananya.xx",
-    content: "Weekend vibes with filter coffee and a book ☕📖 #RelaxMode",
-    image: "https://images.unsplash.com/photo-1517685352821-92cf88aee5a5?auto=format&fit=crop&w=800&q=60" 
+    "username": "ananya.xx",
+    "content": "Weekend vibes with filter coffee and a book ☕📖 #RelaxMode",
+    "image": "https://images.unsplash.com/photo-1517685352821-92cf88aee5a5?auto=format&fit=crop&w=800&q=60"
   },
   {
-    username: "priya._",
-    content: "New earrings just dropped! 💫👂 #DesiSwag",
-    image: "https://images.unsplash.com/photo-1658072800483-e8440abd682f?q=80"
+    "username": "priya._",
+    "content": "New earrings just dropped! 💫👂 #DesiSwag",
+    "image": "https://images.unsplash.com/photo-1658072800483-e8440abd682f?q=80"
   },
   {
-    username: "kiara.luvs",
-    content: "Rainy days and soft music 🎧🌧️ #ChillVibes",
-    image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=60"
+    "username": "kiara.luvs",
+    "content": "Rainy days and soft music 🎧🌧️ #ChillVibes",
+    "image": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=60"
   },
   {
-    username: "rohan_09",
-    content: "My latest mandala art is finally done! 🎨🖌️ #ArtTherapy",
-    image: "https://images.unsplash.com/photo-1605296867304-46d5465a13f1?auto=format&fit=crop&w=800&q=60" 
+    "username": "rohan_09",
+    "content": "My latest mandala art is finally done! 🎨🖌️ #ArtTherapy",
+    "image": "https://images.unsplash.com/photo-1605296867304-46d5465a13f1?auto=format&fit=crop&w=800&q=60"
   },
   {
-    username: "naina.codes",
-    content: "Late-night debugging sessions hit different 😴💻 #TechLife",
-    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeqf_hUkPa9Xh5PmY0NBWppKJxW9sAD1OmVA&sttps://images.unsplash.com/photo-1610563166156-6cdbb1e3b953?auto=format&fit=crop&w=800&q=80"
-  }
-  ,
-  {
-    username: "isha_07",
-    content: "Throwback to that sunset by the lake 🌅❤️ #PeacefulMoments",
-    image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=60" 
+    "username": "naina.codes",
+    "content": "Late-night debugging sessions hit different 😴💻 #TechLife",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeqf_hUkPa9Xh5PmY0NBWppKJxW9sAD1OmVA&sttps://images.unsplash.com/photo-1610563166156-6cdbb1e3b953?auto=format&fit=crop&w=800&q=80"
   },
   {
-    username: "riya.in",
-    content: "Trying my hand at baking today 🍰😄 #ChefModeOn",
-    image: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=800&q=60" 
+    "username": "isha_07",
+    "content": "Throwback to that sunset by the lake 🌅❤️ #PeacefulMoments",
+    "image": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    "username": "riya.in",
+    "content": "Trying my hand at baking today 🍰😄 #ChefModeOn",
+    "image": "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    "username": "Mahak-codes",
+    "image": "https://images.unsplash.com/photo-1503437313881-503a91226402?w=600",
+    "content": "Do what is great!"
   }
 ];
+
 module.exports = posts;
